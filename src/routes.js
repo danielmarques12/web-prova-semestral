aea2ef40ee5df5844a7c6b8502696a309fdd8a2b
Daniel Marques <daniel.brz2009@gmail.com>
2021-06-09 00:22:50 -0300

    Routes

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Home from './pages/Home';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route restricted exact path="/login" component={Login} />
-        <Route restricted exact path="/signup" component={SignUp} />
+        <Route restricted exact path="/" component={Home} />
+        <Route restricted path="/login" component={Login} />
+        <Route restricted path="/signup" component={SignUp} />
       </Switch>
     </BrowserRouter>
   );
