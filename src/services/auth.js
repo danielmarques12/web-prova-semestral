1f567313946039c2c42298fc8be790daf07cf1c4
Daniel Marques <daniel.brz2009@gmail.com>
2021-06-09 00:21:31 -0300

    auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,3 +3,4 @@ export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY);
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = (token) => localStorage.setItem(TOKEN_KEY, token);
 export const logout = () => localStorage.removeItem(TOKEN_KEY);
+export const setUserType = (type) => localStorage.setItem('userType', type);
